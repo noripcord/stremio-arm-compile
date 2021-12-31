274167749a99ce1c03acce513a2e1278b125071a
noripcord <santaf@gmail.com>
2021-12-30 19:37:42 -0500

    Update debian_dependencies.sh
    -yes -yes -yes!

--- debian_dependencies.sh
+++ debian_dependencies.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 apt update
-apt install libqt5dbus5
-sudo apt install libmpv-dev
-apt install libqt5quick5
-apt install nodejs
+apt-get -y install libqt5dbus5
+apt-get -y install libmpv-dev
+apt-get -y install libqt5quick5
+apt-get -y install nodejs
 # TODO: link nodejs (ln -s) in the build folder
 apt-get -y install libkf5webengineviewer-dev
 apt-get -y install qml-module-qtwebchannel qml-module-qt-labs-platform qml-module-qtwebengine qml-module-qtquick-dialogs qml-module-qtquick-controls qtdeclarative5-dev qml-module-qt-labs-settings qml-module-qt-labs-folderlistmodel
